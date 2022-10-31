30319d38aca799cd855f4e215421955f7e6dc873
polydorides.andreas@gmail.com <polydorides.andreas@gmail.com>
2022-10-31 00:03:24 +0000

    adds footer

--- views.py
+++ views.py
@@ -1,10 +1,23 @@
 from flask import Blueprint, render_template, request, jsonify, redirect, url_for
 
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+import folium
+import matplotlib.pyplot as plt
+from scipy.stats import linregress
+
 views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
     return render_template("index.html", name="Andreas")
+
+@views.route("/")
+def plot():
+    world_filepath = gpd.datasets.get_path('naturalearth_lowres')
+    world = gpd.read_file(world_filepath)
+    world.explore()
 
 @views.route("/profile")
 def profile():
