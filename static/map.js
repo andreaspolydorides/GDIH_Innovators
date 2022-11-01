bd563f2b8a76434273662974a4fd44128dbee651
polydorides.andreas@gmail.com <polydorides.andreas@gmail.com>
2022-11-01 00:17:52 +0000

    fixes pan out of map bounds and adds max zoom out

--- static/map.js
+++ static/map.js
@@ -2,7 +2,17 @@ var map = L.map('map').setView([55, 0], 4);
 
 var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
+    minZoom: 2,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+var southWest = L.latLng(-89.98155760646617, -180),
+northEast = L.latLng(89.99346179538875, 180);
+var bounds = L.latLngBounds(southWest, northEast);
+
+map.setMaxBounds(bounds);
+map.on('drag', function() {
+    map.panInsideBounds(bounds, { animate: false });
+});
 
 L.geoJson(countriesData).addTo(map);
