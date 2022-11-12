43f0dce8fec55c9cf0bc0dde06a9292b754c8108
andreaspolydorides <polydorides.andreas@gmail.com>
2022-11-12 13:58:53 +0000

    adds search box (not functional)

--- static/map.js
+++ static/map.js
@@ -6,6 +6,12 @@ var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+map.zoomControl.remove();
+
+L.control.zoom({
+    position: 'bottomright'
+}).addTo(map);
+
 var southWest = L.latLng(-89.98155760646617, -180),
 northEast = L.latLng(89.99346179538875, 180);
 var bounds = L.latLngBounds(southWest, northEast);
@@ -16,3 +22,15 @@ map.on('drag', function() {
 });
 
 L.geoJson(countriesData).addTo(map);
+
+let searchDropdown = '<select name="select_box" class="form-select" id="select_box"><option value="">Select Country</option>';
+
+countryList.forEach(makeSearchHTML);
+
+function makeSearchHTML(item) {
+    searchDropdown += '<option value="' + item + '">' + item + '</option>';
+};
+
+searchDropdown += '</select>';
+
+document.getElementById("countrySearchContainer").innerHTML = searchDropdown;
--- views.py
+++ views.py
@@ -1,37 +1,8 @@
 from flask import Blueprint, render_template, request, jsonify, redirect, url_for
 
-import numpy as np
-import pandas as pd
-import geopandas as gpd
-import folium
-import matplotlib.pyplot as plt
-from scipy.stats import linregress
 
 views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
     return render_template("map.html")
-
-@views.route("/")
-def plot():
-    world_filepath = gpd.datasets.get_path('naturalearth_lowres')
-    world = gpd.read_file(world_filepath)
-    world.explore()
-
-@views.route("/profile")
-def profile():
-    return render_template("profile.html")
-
-@views.route("/json")
-def get_json():
-    return jsonify({'name': 'tim', 'coolness': 10})
-
-@views.route("/data")
-def get_data():
-    data = request.json
-    return jsonify(data)
-
-@views.route("go-to-home")
-def go_to_home():
-    return redirect(url_for("views.home"))
